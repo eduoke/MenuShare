e60c8ea65af2b1b3894fccd09df494e447093af6
eduoke <edu_oke@proton.me>
2025-02-20 23:24:30 +0300

    Minor change

--- RestuarantCore/apps/menu/viewsets.py
+++ RestuarantCore/apps/menu/viewsets.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny,    IsAuthenticated
+from .models import Menu
 
-# Create your views here.
+from .serializers import MenuSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all() 
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
--- RestuarantCore/urls.py
+++ RestuarantCore/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+
+from . routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Api Rroutes 
+    path('api/v1', include((router.urls, 'core_api')))
 ]
